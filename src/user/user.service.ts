8bbbcebbce66fd1596cb0268c885d4c416c3edf4
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-07-29 21:43:37 -0300

    feat: serialize

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
+
+    findOne(id: string) {
+        const user = this.repo.findOne({ where: { id } });
+
+        if (!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+
+        return user;
+    }
+
+    async create(email: string, password: string) {
+        const userExists = await this.repo.findOne({ where: { email } });
+
+        if (userExists) {
+            throw new ConflictException('User with this e-mail already exists');
+        }
+
+        return await this.repo.save({ email, password });
+    }
+
+}
